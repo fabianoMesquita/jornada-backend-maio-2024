9a89dcafe4546158a81eab35219700b96d539dc6
Fabiano Mesquita Silva <111099283+fabianoMesquita@users.noreply.github.com>
2024-05-14 17:10:57 +0000

    Read All Implementado

--- index.js
+++ index.js
@@ -9,4 +9,13 @@ app.get('/oi', function(req,res) {
   res.send('Olá, Mundo!')
 })
 
+//Lista de Itens
+const itens = ['Rick Sanchez', 'Summer Smith', 'Morty Smith']
+
+//End Point Read-All
+
+app.get('/item', function (req,res) {
+  res.send(itens)
+})
+
 app.listen(3000)
